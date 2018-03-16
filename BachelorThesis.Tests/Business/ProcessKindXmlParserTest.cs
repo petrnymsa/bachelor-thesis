35ec229937945062fbe83577cc2885a7c503bff5
Petr Nymsa <nymsapetr@outlook.cz>
2018-03-16 14:38:27 +0100

    Added ProcessKindXmlParser test

---
+++ BachelorThesis.Tests/Business/ProcessKindXmlParserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BachelorThesis.Business.Parsers;
+using NUnit.Framework;
+
+namespace BachelorThesis.Tests.Business
+{
+    [TestFixture]
+    public class ProcessKindXmlParserTest
+    {
+        [Test]
+        public void ParseDefinition_ValidXml_ShouldReturnCorrectResult()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, @"Business/XmlTestFiles/definition-valid.xml");
+            var xDoc = XDocument.Load(path);
+
+            var parser = new ProcessKindXmlParser();
+
+            var result = parser.ParseDefinition(xDoc);
+
+            Assert.NotNull(result.ProcessKind);
+            Assert.NotNull(result.ActorRoles);
+            Assert.That(result.ActorRoles.Count == 4);
+
+        }
+    }
+}
