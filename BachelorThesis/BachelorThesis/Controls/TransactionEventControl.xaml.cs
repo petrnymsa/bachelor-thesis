2907cb3cfea06125fa32c87091d1f2f04bc9288e
Petr Nymsa <nymsapetr@outlook.cz>
2018-04-11 20:17:15 +0200

    Added TransactionEventControl

---
+++ BachelorThesis/BachelorThesis/Controls/TransactionEventControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BachelorThesis.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TransactionEventControl : ContentView
+    {
+        //public static readonly BindableProperty TransactionIdentifierProperty =
+        //    BindableProperty.Create(nameof(TransactionIdentifier), typeof(string), typeof(TransactionEventControl), string.Empty, 
+        //       BindingMode.TwoWay, propertyChanged: (b, o, n) => {    });
+
+        //public string TransactionIdentifier
+        //{
+        //    get => (string)GetValue(TransactionIdentifierProperty);
+        //    set => SetValue(TransactionIdentifierProperty, value);
+        //}
+
+
+        public static readonly BindableProperty TopLabelProperty = BindableProperty.Create(nameof(TopLabel), typeof(string), typeof(TransactionEventControl), default(string));
+
+        public string TopLabel
+        {
+            get => (string)GetValue(TopLabelProperty);
+            set => SetValue(TopLabelProperty, value);
+        }
+
+        public static readonly BindableProperty BottomLabelProperty = BindableProperty.Create(nameof(BottomLabel), typeof(string), typeof(TransactionEventControl), default(string));
+
+        public string BottomLabel
+        {
+            get => (string)GetValue(BottomLabelProperty);
+            set => SetValue(BottomLabelProperty, value);
+        }
+
+
+        public TransactionEventControl ()
+		{
+			InitializeComponent ();
+
+            
+
+		    //this.Content = new StackLayout()
+		    //{
+		    //    Children =
+		    //    {
+		    //        new Label() {Text = "T1[pm]"},
+		    //        new BoxView() {Color = Color.LightBlue, WidthRequest = 20, HorizontalOptions = LayoutOptions.Center},
+		    //        new Label() {Text = "8.4"},
+		    //        new Label() {Text = "14:12:42"}
+		    //    }
+		    //};
+		}
+	}
+}
--- BachelorThesis/BachelorThesis/TestPage.xaml.cs
+++ BachelorThesis/BachelorThesis/TestPage.xaml.cs
@@ -15,35 +15,11 @@ namespace BachelorThesis
         public TestPage ()
 		{
 			InitializeComponent ();
-
-            //Content = new StackLayout()
-            //{
-            //    BackgroundColor = Color.Azure,
-            //    Padding = 20,
-            //    Children =
-            //    {
-            //        new TransactionBoxControl()
-            //        {
-            //            WidthRequest = 200,
-            //            HeightRequest = 30,
-            //            BackgroundColor = Color.DarkRed,
-            //            HorizontalOptions = LayoutOptions.Start,
-            //            VerticalOptions = LayoutOptions.Start,
-            //            Progress = 0.5f
-            //        }
-            //    }
-            //};
         }
 
 	    private void Button_OnClicked(object sender, EventArgs e)
 	    {
-	       // Progress1.Animate(nameof(Progress1), x => Progress1.EndingDegrees = (float)x, 0, endingAt1, 4, 5000, Easing.BounceIn);
-
-	        var start = boxControl.Progress;
-            boxControl.Animate("ProgrAnim",x=> boxControl.Progress = (float)x,start, 1f,4,2000,Easing.Linear);
-
-	     //   boxControl.Animate("ProgrAnim", x => boxControl.Progress = (float)x, start, 0.0f, 4, 2000, Easing.Linear);
-
-        }
-    }
+	        eventControl.TransactionIdentifier = transactionId.Text;
+	    }
+	}
 }
