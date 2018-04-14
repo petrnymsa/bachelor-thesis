5c7fb3c580e680f523eff98e626c3c6e607122c7
Petr Nymsa <nymsapetr@outlook.cz>
2018-04-14 17:01:06 +0200

    SimulationCases merged and moved to one assembly

--- BachelorThesis/BachelorThesis/Views/ProcessVisualisationPage.xaml.cs
+++ BachelorThesis/BachelorThesis/Views/ProcessVisualisationPage.xaml.cs
@@ -45,35 +45,11 @@ namespace BachelorThesis.Views
             base.OnAppearing();
             MessagingCenter.Send(this, "setLandscape");
 
-            //if (livePreview)
-            //{
-            //    var box = transactionBoxControls.Find(x => x.TransactionId == 1);
-            //    var offset = timeLineLayout.X - 100;
-            //    var spaceX = box.X - offset;
-            //    var move = spaceX + box.GetCompletionPosition(TransactionCompletion.Requested);
-
-            //    //timeLineLayout.Children.Add(new BoxView()
-            //    //    {
-            //    //        Color = Color.Black,
-            //    //        HeightRequest = 10,
-            //    //        WidthRequest = 5
-
-            //    //    }, xConstraint: Constraint.RelativeToParent((p) => move - 100)
-            //    //);
-            //}
-           // else 
             await PrepareSimulation();
         }
 
         private async Task PrepareSimulation()
         {
-            //var assembly = typeof(ProcessVisualisationPage).GetTypeInfo().Assembly;
-            //Stream stream = assembly.GetManifestResourceStream("BachelorThesis.SimulationFiles.case-02.xml");
-
-          
-            //foreach (var res in typeof(IntExtension).GetTypeInfo().Assembly.GetManifestResourceNames())
-            //    System.Diagnostics.Debug.WriteLine("[info] found resource: " + res);
-
             var assembly = typeof(SimulationCases).GetTypeInfo().Assembly;
             Stream stream = assembly.GetManifestResourceStream(SimulationCases.Case01);
 
@@ -94,17 +70,6 @@ namespace BachelorThesis.Views
 
         private void BtnNextStep_OnClicked(object sender, EventArgs e)
         {
-            //if (livePreview)
-            //{
-            //    foreach (var box in transactionBoxControls)
-            //    {
-            //        var start = box.Progress;
-            //        var end = start + 0.20f;
-            //        box.Animate("aa", x => box.Progress = (float) x, start, end, 4, 1200, Easing.SinInOut);
-            //    }
-            //    return;
-            //}
-
             var results = rentalContractSimulation.SimulateNextChunk();
 
             if (results == null)
@@ -127,33 +92,9 @@ namespace BachelorThesis.Views
 
                 DebugHelper.Info($"box: {transactionControl.X}, timeline: {timeLineLayout.X}, offset: {offset}, spaceX: {spaceX}, move: {move}");
 
-                //var eventControl = new TransactionEventControl()
-                //{
-                //   // BackgroundColor = Color.Azure,
-                    
-                //    TopLabel = $"{transaction.Identificator}[{evtCompletion.Completion.AsAbbreviation()}]",
-                //  //  BottomLabel = $"{evt.Created.Day}.{evt.Created.Month} {evt.Created.Hour}:{evt}",
-                //    BottomLabel = evt.Created.ToString("dd.MM HH:mm:ss"),
-                //    Completion = evtCompletion.Completion
-
-                //};
-
-                //transactionControl.AssociateEvent(eventControl);
-
+              
                var timeLineEvent = timeLineLayout.AddEvent(move, transaction.Identificator, evtCompletion, transactionControl.HighlightColor);
                 transactionControl.AssociateEvent(timeLineEvent, evtCompletion.Completion);
-
-                //timeLineLayout.Children.Add(eventControl,
-                //    xConstraint: Constraint.RelativeToParent(p => move - eventControl.Width / 2),
-                //    yConstraint: Constraint.RelativeToParent(p => p.Height * 0.5f - eventControl.HeightRequest / 2f));
-                //timeLineLayout.Children.Add(new BoxView()
-                //    {
-                //        Color = Color.Azure,
-                //        WidthRequest = 3,
-                //        HeightRequest = 40
-
-                //    }, xConstraint: Constraint.RelativeToParent((p) => move )
-                //);
             }
 
         }
@@ -170,20 +111,7 @@ namespace BachelorThesis.Views
 
         private void ScrollView_OnScrolled(object sender, ScrolledEventArgs e)
         {
-            //Debug.WriteLine($"[info] Scroll x: {e.ScrollX} y: {e.ScrollY}");
-            //Debug.WriteLine($"[info] timeLine y: {timeLineLayout.Y}");
-            //Debug.WriteLine($"[info] main y: {mainLayout.Y}");
-
-            // timeLineLayout.TranslateTo(mainLayout.Y + e.ScrollY, e.ScrollY, 0);
-
             RelativeLayout.SetYConstraint(timeLineLayout, Constraint.RelativeToParent((parent) => parent.Y + e.ScrollY));
-         //   RelativeLayout.SetYConstraint(timeLineLayout, Constraint.RelativeToParent((parent) => parent.Y + e.ScrollY));
-            // RelativeLayout.SetYConstraint(timeLineLayout, Constraint.RelativeToParent((parent) => parent.Y + e.ScrollY));
         }
-
-//        private void BtnTest_OnClicked(object sender, EventArgs e)
-//        {
-//            Navigation.PushAsync(new TestPage());
-//        }
     }
 }
