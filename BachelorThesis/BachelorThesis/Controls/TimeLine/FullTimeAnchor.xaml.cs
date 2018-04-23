57b0aeb13e1d022e6b3c7b85c4fee8df240105be
Petr Nymsa <nymsapetr@outlook.cz>
2018-04-23 21:57:20 +0200

    Added FullTime anchor

---
+++ BachelorThesis/BachelorThesis/Controls/TimeLine/FullTimeAnchor.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BachelorThesis.Business.DataModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BachelorThesis.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FullTimeAnchor : TimeLineEventAnchor
+    {
+	    public static readonly BindableProperty HourProperty = BindableProperty.Create(nameof(Hour), typeof(int), typeof(FullTimeAnchor), 0);
+
+	    public int Hour
+	    {
+	        get => (int)GetValue(HourProperty);
+	        set { SetValue(HourProperty, value); OnPropertyChanged(nameof(FormattedString)); }
+	    }
+
+
+	    public static readonly BindableProperty MinuteProperty = BindableProperty.Create(nameof(Minute), typeof(int), typeof(FullTimeAnchor), 0);
+
+	    public int Minute
+	    {
+	        get => (int) GetValue(MinuteProperty);
+	        set
+	        {
+	            SetValue(MinuteProperty, value);
+	            OnPropertyChanged(nameof(FormattedString));
+	        }
+
+	    }
+
+	    public static readonly BindableProperty SecondProperty =
+	        BindableProperty.Create(nameof(Seconds), typeof(int), typeof(SecondAnchor), default(int));
+
+	    public int Seconds
+	    {
+	        get => (int)GetValue(SecondProperty);
+	        set { SetValue(SecondProperty, value); OnPropertyChanged(nameof(FormattedString)); }
+	    }
+
+	    public string FormattedString => $"{Hour:00}:{Minute:00}:{Seconds:00}";
+
+        public FullTimeAnchor (int hour, int minute, int seconds, double leftX, TimeLineEvent timeEvent, TransactionCompletion completion)
+        :base(leftX, timeEvent, completion)
+		{
+			InitializeComponent ();
+
+		    Hour = hour;
+		    Minute = minute;
+		    Seconds = seconds;
+		}
+	}
+}
---
+++ BachelorThesis/BachelorThesis/Controls/TimeLine/TimeLineEventAnchor.cs
@@ -0,0 +1,26 @@
+﻿using BachelorThesis.Business.DataModels;
+using Xamarin.Forms;
+
+namespace BachelorThesis.Controls
+{
+    public class TimeLineEventAnchor : TimeLineAnchor
+    {
+        public TransactionCompletion Completion { get; set; }
+        public TimeLineEvent Event { get; set; }
+
+
+        public static readonly BindableProperty WithEventProperty = BindableProperty.Create(nameof(WithEvent), typeof(bool), typeof(TimeLineEventAnchor), true);
+
+        public bool WithEvent
+        {
+            get => (bool)GetValue(WithEventProperty);
+            set => SetValue(WithEventProperty, value);
+        }
+
+        public TimeLineEventAnchor(double leftX, TimeLineEvent timeEvent, TransactionCompletion completion) : base(leftX)
+        {
+            Event = timeEvent;
+            Completion = completion;
+        }
+    }
+}
