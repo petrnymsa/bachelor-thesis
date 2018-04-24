4d288da74eabb0747810cf5263dc867a63724a7a
Petr Nymsa <nymsapetr@outlook.cz>
2018-04-24 10:17:55 +0200

    TransactionBoxControl code cleaning

--- BachelorThesis/BachelorThesis/Controls/TransactionBoxControl.cs
+++ BachelorThesis/BachelorThesis/Controls/TransactionBoxControl.cs
@@ -14,50 +14,50 @@ namespace BachelorThesis.Controls
 {
     public class TransactionBoxControl : SKCanvasView
     {
-        class Anchor
-        {
-            public const float AnchorWidth = 8f;
+        //class Anchor
+        //{
+        //    public const float AnchorWidth = 8f;
 
-            public float X { get; }
-            private bool isFocused;
+        //    public float X { get; }
+        //    private bool isFocused;
 
-            public bool IsFocused
-            {
-                get => isFocused;
-                set
-                {
-                    isFocused = value;
-                  //  AssociatedEvent.IsRevealed = isFocused;
-                }
-            }
+        //    public bool IsFocused
+        //    {
+        //        get => isFocused;
+        //        set
+        //        {
+        //            isFocused = value;
+        //          //  AssociatedEvent.IsRevealed = isFocused;
+        //        }
+        //    }
 
-            private TimeLineAnchor AssociatedEvent { get; }
+        //    private TimeLineAnchor AssociatedEvent { get; }
 
-            public Anchor(float x, TimeLineAnchor associatedEvent, bool isFocused = false)
-            {
-                X = x;
-                AssociatedEvent = associatedEvent;
-                IsFocused = isFocused;
+        //    public Anchor(float x, TimeLineAnchor associatedEvent, bool isFocused = false)
+        //    {
+        //        X = x;
+        //        AssociatedEvent = associatedEvent;
+        //        IsFocused = isFocused;
 
-            }
+        //    }
 
-            public bool HitTestX(SKPoint p, float tolerance = 10f)
-            {
-                return Math.Abs(X - p.X) <= tolerance;
-            }
-        }
+        //    public bool HitTestX(SKPoint p, float tolerance = 10f)
+        //    {
+        //        return Math.Abs(X - p.X) <= tolerance;
+        //    }
+        //}
 
-        class BoxDescendant
-        {
-            public TransactionBoxControl Control { get; set; }
-            public TransactionCompletion OffsetCompletion { get; set; }
+        //class BoxDescendant
+        //{
+        //    public TransactionBoxControl Control { get; set; }
+        //    public TransactionCompletion OffsetCompletion { get; set; }
 
-            public BoxDescendant(TransactionBoxControl control, TransactionCompletion offsetCompletion)
-            {
-                Control = control;
-                OffsetCompletion = offsetCompletion;
-            }
-        }
+        //    public BoxDescendant(TransactionBoxControl control, TransactionCompletion offsetCompletion)
+        //    {
+        //        Control = control;
+        //        OffsetCompletion = offsetCompletion;
+        //    }
+        //}
 
         #region BindableProperties
         public static BindableProperty ProgressProperty =
@@ -145,49 +145,24 @@ namespace BachelorThesis.Controls
 
         public TransactionInstance Transaction { get; set; }
         public TransactionBoxControl ParentControl { get; set; }
-        //        public float OffsetX { get; set; }
-        //
-        //        public float OffsetX
-        //        {
-        //            get
-        //            {
-        //                if (ParentControl != null)
-        //                    return (float) ParentControl.X + OffsetX;
-        //
-        //                return OffsetX;
-        //            }
-        //        }
 
         #endregion
 
-        private List<TransactionBoxControl> descendats { get; set; }
-
+        private bool stopped = false;
         private const float BorderWidth = 3f;
+        private readonly List<TransactionBoxControl> descendats;
+        private readonly List<TransactionLinkControl> links;
 
-        private List<TransactionLinkControl> links;
 
         public TransactionBoxControl()
         {
             HorizontalOptions = LayoutOptions.Start;
             VerticalOptions = LayoutOptions.Start;
 
-            EnableTouchEvents = true;
-            Touch += OnTouch;
 
-        //    events = new Dictionary<Guid, List<Anchor>>();
             descendats = new List<TransactionBoxControl>();
             links = new List<TransactionLinkControl>();
-         //   completions = new Dictionary<TransactionCompletion, float>();
         }
-
-      //  private readonly Dictionary<Guid, List<Anchor>> events;
-      //  private Dictionary<TransactionCompletion, float> completions;
-
-    //    private SKPoint lastTouch;
-
-     //   private bool isPressed = false;
-        private bool stopped = false;
-        private SKImageInfo info;
 
         protected float AsPixel(double input)
         {
@@ -200,10 +175,7 @@ namespace BachelorThesis.Controls
         {
             //base.OnPaintSurface(e);
 
-            info = e.Info;
             var canvas = e.Surface.Canvas;
-
-            var formSize = new Size(WidthRequest, HeightRequest);
 
             var paintBorder = new SKPaint
             {
@@ -226,7 +198,7 @@ namespace BachelorThesis.Controls
             //  var scaleFactor = (float)(e.Info.Width / this.Width);
             canvas.Clear();
             //  canvas.Scale(scaleFactor);
-            canvas.Save();
+            //  canvas.Save();
             if (!IsActive)
                 paintBorder.PathEffect = SKPathEffect.CreateDash(new float[] { 10, 5 }, 1);
             // main rectangle
@@ -236,82 +208,18 @@ namespace BachelorThesis.Controls
                 canvas.DrawRect(new SKRect(BorderWidth, BorderWidth, AsPixel(Progress * WidthRequest) - BorderWidth, AsPixel(HeightRequest) - BorderWidth), paintProgress);
 
             //  progress helpers
-            paintProgress.Color = Color.Chocolate.ToSKColor();
-            paintProgress.StrokeWidth = 2;
-            for (int i = 1; i <= 5; i++)
-            {
-                var cmp = (TransactionCompletion)i;
-                canvas.Save();
-                canvas.Translate(AsPixel(GetCompletionPosition(cmp)), 0);
-                canvas.DrawLine(0, 1, 0, info.Height - 2, paintProgress);
-                canvas.Restore();
-            }
-            canvas.Restore();
+            //            paintProgress.Color = Color.Chocolate.ToSKColor();
+            //            paintProgress.StrokeWidth = 2;
+            //            for (int i = 1; i <= 5; i++)
+            //            {
+            //                var cmp = (TransactionCompletion)i;
+            //                canvas.Save();
+            //                canvas.Translate(AsPixel(GetCompletionPosition(cmp)), 0);
+            //                canvas.DrawLine(0, 1, 0, info.Height - 2, paintProgress);
+            //                canvas.Restore();
+            //            }
+            //            canvas.Restore();
 
-
-//            using (SKPaint anchorPaint = new SKPaint())
-//            {
-//                anchorPaint.Style = SKPaintStyle.Fill;
-//                anchorPaint.Color = HighlightColor.ToSKColor();
-//
-//                foreach (var anchorList in events)
-//                {
-//                    foreach (var anchor in anchorList.Value)
-//                    {
-//                        var size = anchor.IsFocused ? Anchor.AnchorWidth * 2 : Anchor.AnchorWidth;
-//                        canvas.Save();
-//                        canvas.Translate(AsPixel(anchor.X), AsPixel(HeightRequest / 2f));
-//                        canvas.RotateDegrees(45, 0, 0);
-//                        //canvas.DrawCircle(anchor.X, info.Height / 2f, size, paintTouchPoint);
-//                        canvas.DrawRect(new SKRect(-size / 2f, -size / 2f, size, size), anchorPaint);
-//                        canvas.Restore();
-//                    }
-//                }
-//            }
-
-        }
-
-        private void OnTouch(object sender, SKTouchEventArgs e)
-        {
-            //   var lastTouch = SKPoint.Empty;
-//            switch (e.ActionType)
-//            {
-//                case SKTouchAction.Pressed:
-//                    lastTouch = e.Location;
-//                    isPressed = true;
-//                    //     touches.Add(lastTouch);
-//                    break;
-//                case SKTouchAction.Moved:
-//                    lastTouch = e.Location;
-//                    break;
-//                case SKTouchAction.Released:
-//                    isPressed = false;
-//                    break;
-//            }
-//
-//            e.Handled = true;
-//
-//            if (isPressed)
-//            {
-//                //foreach (var anchor in anchors)
-//                //{
-//                //    var collision = anchor.HitTestX(lastTouch, 20);
-//                //}
-//
-//                foreach (var eventAnchors in events)
-//                {
-//                    foreach (var anchor in eventAnchors.Value)
-//                    {
-//                        var collision = anchor.HitTestX(lastTouch, 20);
-//                        anchor.IsFocused = collision;
-//                        if (collision)
-//                            break;
-//                    }
-//                }
-//            }
-//            else events.ForEach(x => x.Value.ForEach(an => an.IsFocused = false));
-//
-//            ((SKCanvasView)sender).InvalidateSurface();
         }
 
         public void AddProgress(TransactionCompletion completion)
@@ -325,48 +233,10 @@ namespace BachelorThesis.Controls
             this.Animate("ProgressAnimation", x => Progress = (float)x, start, end, 4, 1200, Easing.SinInOut);
         }
 
-        //        public void AddProgress(float end)
-        //        {
-        //            var start = Progress;
-        //            this.Animate("ProgressAnimation", x => Progress = (float)x, start, end, 4, 1200, Easing.SinInOut);
-        //        }
-
-        public void AssociateEvent(TimeLineAnchor timeLineAnchor)
-        {
-//            // var percent = completion.ToPercentValue();
-//            //var percent = Progress + 0.2f;
-//            if (!events.ContainsKey(timeLineAnchor.Id))
-//                events[timeLineAnchor.Id] = new List<Anchor>();
-//            // var x = (float)timeLineAnchor.X - OffsetX - 100;
-//            var x = timeLineAnchor.GetXPositionWithoutOffsets() - (float)X;
-//
-//            //  AddProgress(x);
-//
-//            //    completions[timeLineAnchor.Completion] = x;
-//
-//            var link = links.FirstOrDefault(l => l.SourceCompletion == timeLineAnchor.Completion);
-//            link?.RefreshLayout();
-//
-//            if (x > WidthRequest) // resize !
-//                WidthRequest += timeLineAnchor.Completion.RemainingAsWidth((float)WidthRequest);
-//
-//            //var x = percent * (float)WidthRequest - Anchor.AnchorWidth / 2f;
-//            //if (completion == TransactionCompletion.Accepted)
-//            //    x -= Anchor.AnchorWidth;
-//            events[timeLineAnchor.Id].Add(new Anchor(x, timeLineAnchor));
-//
-//
-//            InvalidateSurface();
-        }
 
         public float GetCompletionPosition(TransactionCompletion completion)
         {
-            //            if (completions.ContainsKey(completion))
-            //                return completions[completion];
-
-
             var percent = completion.ToPercentValue();
-            // var percent = Progress + 0.2f;
             return ((float)WidthRequest * percent); // - TransactionLinkControl.ShapeRadius;//*2 - TransactionLinkControl.ShapeRadius/2f;
         }
 
@@ -396,26 +266,5 @@ namespace BachelorThesis.Controls
         {
             links.Add(link);
         }
-
-
-        //public void RefreshLayout()
-        //{
-        //    if(!Transaction.RequestedTime.HasValue || !Transaction.ExpectedEndTime.HasValue)
-        //        return;
-
-        //    var totalMinutes = Transaction.ExpectedEndTime.Value.TimeOfDay.TotalMinutes -
-        //                       Transaction.RequestedTime.Value.TimeOfDay.TotalMinutes;
-        //    WidthRequest = totalMinutes * TimeLine.HourMinuteSpacing;
-
-        //    InvalidateSurface();
-        //}
-
-        //public void RefreshLayoutAsSketch()
-        //{
-
-        //    var expectedMinute = Transaction.TransactionKind.ExpectedTimeEstimate;
-        //    WidthRequest = expectedMinute * TimeLine.HourMinuteSpacing;
-        //    InvalidateSurface();
-        //}
     }
 }
